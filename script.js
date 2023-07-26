42604ed1138b9b2a31583bd39829f2ee5cf10ed6
adorable-pixie <wangjin_jean@126.com>
2023-07-26 15:08:45 +0800

    根据鼠标在Y上的移动决定樱花雨势

--- script.js
+++ script.js
@@ -1,93 +1,95 @@
 // 获取canvas元素
 const canvas = document.querySelector('canvas');
 
-// 设置canvas画布的宽高为浏览器视口宽高
+// 设置canvas元素的宽高，为浏览器视口的宽高
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// 使用2d的绘图方式
-const ctx = canvas.getContext('2d');
+//使用2D的绘图方式
+const ctx = canvas.getContext('2d')  //getContext获取绘图环境
 
-// 定义花瓣的数量
-const SAKURA_SUM = 60;
-// 花瓣数组
-const sakuraArray = [];
+//定义花瓣的数量
+let SAKURA_SUM = 60;
+//定义花瓣数组
+const sakuraArray = [];   //存放花瓣对象
 
-/**
- * 定义花瓣类
- */
+// 花瓣类
 class Sakura {
-    // 构造方法
-    constructor() {
-        // 随机生成花瓣的x, y坐标
-        this.x = Math.random() * canvas.width;
+    //构造方法，用来初始化
+    constructor(){
+        //随机生成花瓣的坐标 x，y
+        this.x = Math.random() * canvas.width;  //Math.random随机生成0-1之间的小数
         this.y = (Math.random() * canvas.height * 2) - canvas.height;
-        // 随机生成花瓣的宽高
+        //随机生成宽高
         this.width = Math.random() * 15 + 25;
         this.height = Math.random() * 12 + 20;
-        // 随机透明度
+        //随机透明度
         this.opacity = this.w / 50;
-        // 设置一个随机数，后面实现旋转角度效果时会用到
+        //设置一个随机数，用于控制花瓣旋转
         this.rotate = Math.random();
-        // 速度初始化
+        //速度初始化
         this.xSpeed = Math.random() * 2 + 1;
         this.ySpeed = Math.random() + 1.5;
+        //旋转速度
         this.rotateSpeed = Math.random() * 0.02;
     }
-
-    // 绘制
-    draw() {
-        // 当花瓣超过canvas画布边界后，重新设置花瓣的坐标、速度、和转速
-        if (this.x > canvas.width || this.y > canvas.height) {
-            this.x = -sakuraImg.width; // 刚好藏住
+    //绘制
+    draw(){
+        //当花瓣超过canvas画布边界后，重新设置花瓣的坐标、速度和转速
+        if(this.x >canvas.width || this.y > canvas.height){
+            this.x = -sakuraImg.width; //刚好藏住
             this.y = (Math.random() * canvas.height * 2) - canvas.height;
             this.rotate = Math.random();
-            this.rotateSpeed = Math.random() * 0.02;
             this.xSpeed = Math.random() * 2 + 0.5;
             this.ySpeed = Math.random() + 1;
+            this.rotateSpeed = Math.random() * 0.02;
         }
-        // 设置整个canvas透明度基数
+
+        //设置下整个canvas的透明基数
         ctx.globalAlpha = this.opacity;
         ctx.drawImage(
             sakuraImg,
             this.x,
             this.y,
-            this.width * (0.6 + (Math.abs(Math.cos(this.rotate)) / 3)),
-            this.height * (0.8 + (Math.abs(Math.sin(this.rotate)) / 5))
+            this.width *(0.6 + (Math.abs(Math.cos(this.rotate)) / 3)),//设置图片的宽高，让人从视觉上看起来是旋转了一定的角度
+            this.height * (0.8 + (Math.abs(Math.sin(this.rotate)) / 5)),
         )
     }
-
     animate() {
         this.x += this.xSpeed + mouseX * 5;
         this.y += this.ySpeed + mouseX * 2;
         this.rotate += this.rotateSpeed;
+        //重新绘制
         this.draw();
     }
 }
 
 /**
- * 定义渲染方法
+ * 渲染函数
  */
-function render() {
-    // 清除矩形内的内容 - 清空屏幕
+function render(){
+    //清除矩形坐标内画布的内容
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    sakuraArray.forEach(sakura => sakura.animate());
-    // 该方法会高速浏览器在重绘之前调用指定的函数
-    // 这样可以保证在浏览器的刷新频率下去更新动画；
+
+    //重新绘制
+    sakuraArray.forEach(sakura=>sakura.animate());
+    //该方法会告诉浏览器重绘之前调用指定的函数
+    //这样可以保证在浏览器的刷新频率下去更新动画
     window.requestAnimationFrame(render);
 }
 
-// 加载花瓣图片
-const sakuraImg = new Image();
+//加载花瓣图片
+const sakuraImg = new Image();   //声明一个图片对象
 sakuraImg.src = './image/sakura.png';
-// 等花瓣图片加载完毕，将数目为SAKURA_SUM的花瓣实例保存到数组中
+//监听樱花图片加载完毕的load事件
 sakuraImg.addEventListener('load', () => {
-    for (let i = 0; i < SAKURA_SUM; i++) {
+    //加载完毕执行这个回调函数
+    //等花瓣图片加载完毕，将数目为SAKURA_SUM的花瓣实例保存到数组中
+    for(let i = 0;i < SAKURA_SUM;i++){
         sakuraArray.push(new Sakura())
     }
     render();
 });
-
 
 // 监听浏览器窗口大小变化，重新设置canvas的宽高
 window.addEventListener('resize', () => {
@@ -96,11 +98,29 @@ window.addEventListener('resize', () => {
 });
 
 let mouseX = 0;
-function touchHandler(e) {
-    // clinetX: 客户端区域的水平坐标 (与页面坐标不同)
+function touchHandler(e){
+    //clientX:客户端区域的水平座标（与页面坐标不同）
     mouseX = (e.clientX || e.touches[0].clientX) / window.innerWidth;
+    //根据鼠标在y轴上的偏移量，设置樱花雨下的大不大
+    //计算樱花花瓣数量相对60个增加多少
+    SAKURA_SUM_DELTA = Math.floor(((e.clientY || e.touches[0].clientY) / window.innerHeight)*120);
+    //当前期望的花瓣数量SAKURA_SUM_DELTA+SAKURA_SUM与此时实际花瓣数量sakuraArray.length的差值ArraylenDelta
+    ArraylenDelta=SAKURA_SUM_DELTA+SAKURA_SUM-sakuraArray.length
+    if(ArraylenDelta>0){
+        //期望雨下的更大
+        for (let i = 0; i < ArraylenDelta; i++) {
+            sakuraArray.push(new Sakura())
+        }
+    }else{
+        //期望雨变小
+        for (let i = 0; i < Math.abs(ArraylenDelta); i++) {
+            item=sakuraArray.pop(new Sakura())
+            delete item;
+        }
+    }
 }
-// mousemove：鼠标移动的事件
-window.addEventListener('mousemove', touchHandler);
-// touchmove；触点在触屏上移动的事件
-window.addEventListener('touchmove', touchHandler);
+
+//监听鼠标移动事件
+window.addEventListener('mousemove',touchHandler);
+//监听触点在触屏上移动事件
+window.addEventListener('touchmove',touchHandler);
